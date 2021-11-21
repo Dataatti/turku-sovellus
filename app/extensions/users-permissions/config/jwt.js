3a1543030b875fcf7779ddea55108003b1c3daa6
Matias <matias.lappalainen@loikka.io>
2021-11-21 16:43:29 +0200

    Modify docker-compose and move it to other directory

--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'e8312483-92ac-4154-be05-6afe3d1d7066'
-};
---
+++ strapi/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5808a834776bce61f87ceb44c74d2985'),
+    },
+  },
+});
