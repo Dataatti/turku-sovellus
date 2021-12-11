8ec8dc1c0a3c61ce390423aace43edbb1f7cee39
Petro Silenius <petro@dataatti.io>
2021-12-11 19:06:54 +0200

    Test language switching with Cypress

--- client/cypress/integration/index.spec.js
+++ client/cypress/integration/index.spec.js
@@ -5,4 +5,16 @@ describe('index', () => {
     cy.get('[data-testid=top-bar] h1').should('have.text', '');
     cy.get('[data-testid=top-bar] [alt="Turku logo"]').should('be.visible');
   });
+
+  it('should change content language', () => {
+    cy.visit('/');
+    cy.get('[data-testid=language-select]').click();
+    cy.get('[data-value="en"]').click();
+    cy.url().should('include', '/en');
+    // TODO: Check content language
+    cy.get('[data-testid=language-select]').click();
+    cy.get('[data-value="sv"]').click();
+    cy.url().should('include', '/sv');
+    // TODO: Check content language
+  });
 });
