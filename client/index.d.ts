e7357287fc54bf8eb5cafc84c25e24215e9be7d3
Petro Silenius <petro@dataatti.io>
2021-12-13 00:20:13 +0200

    Add types for languages and Kerrokantasi hearings

---
+++ client/index.d.ts
@@ -0,0 +1,30 @@
+type Lang = 'fi' | 'en' | 'sv';
+
+interface MultiLangContent {
+  fi?: string;
+  en?: string;
+  sv?: string;
+}
+
+// Kerrokantasi Hearing
+interface Hearing {
+  id: string;
+  n_comments: number;
+  open_at: string;
+  close_at: string;
+  published: boolean;
+  closed: boolean;
+  title: MultiLangContent;
+  abstract: MultiLangContent;
+  slug: string;
+  main_image: {
+    alt_text: MultiLangContent;
+    caption: MultiLangContent;
+    height: number;
+    width: number;
+    id: string;
+    title: MultiLangContent;
+    url: string;
+  };
+  labels: { id: number; label: MultiLangContent }[];
+}
