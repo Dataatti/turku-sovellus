ea80788929518fe7fff36a89354ac0c77c673cfa
Aleksi Väisänen <aleksivaisa@gmail.com>
2021-12-12 16:56:19 +0200

    Update theme.ts and tsconfig.json

--- client/theme.ts
+++ client/theme.ts
@@ -1,5 +1,29 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({});
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Arial'].join(','),
+    h1: {
+      fontSize: 24,
+    },
+    h2: {
+      fontSize: 20,
+    },
+    h3: {
+      fontSize: 18,
+    },
+    body1: {
+      fontSize: 16
+    }
+  },
+  palette: {
+    primary: {
+      main: '#006BB6',
+    },
+    secondary: {
+      main: '#F26522',
+    },
+  },
+});
 
 export default theme;
