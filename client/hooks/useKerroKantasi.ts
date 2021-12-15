f0e99660e310c2199821554b64e75f4bfb17340a
Petro Silenius <petro@dataatti.io>
2021-12-15 08:21:24 +0200

    Fetch only open hearings

--- client/hooks/useKerroKantasi.ts
+++ client/hooks/useKerroKantasi.ts
@@ -1,9 +1,9 @@
 import { useQuery } from 'react-query';
 
 const getKerroKantasi = async () => {
-  const response = await fetch('https://kerrokantasi-api.turku.fi/v1/hearing');
+  const response = await fetch('https://kerrokantasi-api.turku.fi/v1/hearing?open=true');
   const data = await response.json();
-  const results: Hearing[] = data.results.filter((hearing: Hearing) => hearing.closed === false);
+  const results: Hearing[] = data.results;
   return results;
 };
 
