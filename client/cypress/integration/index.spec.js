74a00fc6b9f9e8967ebb957a2dfd8c5bd364f957
Petro Silenius <petro@dataatti.io>
2021-12-19 15:22:11 +0200

    Check for liikennetiedotteet content in index page cypress test

--- client/cypress/integration/index.spec.js
+++ client/cypress/integration/index.spec.js
@@ -1,22 +1,26 @@
 describe('index', () => {
   it('should load the index page', () => {
-    
+
     cy.intercept({
       method: 'GET',
       url: 'https://kerrokantasi-api.turku.fi/**',
     }).as('kerrokantasiData');
-    
+
     cy.intercept({
       method: 'GET',
       url: 'https://api.turku.fi/linkedevents/v1/**',
     }).as('eventsData');
     cy.visit('/');
     cy.wait(['@kerrokantasiData', '@eventsData']);
-    
+
     cy.get('[data-testid="turussa-tapahtuu-widget"]')
       .find('li')
       .should('have.length.greaterThan', 0);
+
     cy.get('[data-testid="kerro-kantasi-widget"]').find('li').should('have.length.greaterThan', 0);
+
+    cy.get('[data-testid="liikenne-tiedotteet-widget"] iframe[data-widget-id="profile:Turunliikenne"]').should('be.visible');
+
     cy.get('[data-testid=top-bar] h1').should('have.text', '');
     cy.get('[data-testid=top-bar] [alt="Turku logo"]').should('be.visible');
   });
