474b4ea7723123822225155063384033840a3c9b
Petro Silenius <petro@dataatti.io>
2021-12-20 00:19:30 +0200

    Check if nosto has image or not

--- client/components/nostot/NostotWidget.tsx
+++ client/components/nostot/NostotWidget.tsx
@@ -1,18 +1,18 @@
 import ListWidget from 'components/ListWidget';
 import { useNostot } from 'hooks/useNostot';
 
-export const NostotWidget = ({ locale, title }: { locale: Lang; title: string }) => {
+export const NostotWidget = ({ title }: { title: string }) => {
   const { isLoading, data } = useNostot();
 
   const mapData = (data: Nosto[]) => {
     const mappedNostot = data.map((nosto) => {
-      const thumbnail = nosto.attributes.header_image.data.attributes.formats.thumbnail;
+      const thumbnail = nosto.attributes.header_image.data?.attributes.formats.thumbnail;
       return {
         title: nosto.attributes.title,
         description: nosto.attributes.description,
         thumbnail: {
-          src: process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url || '',
-          alt: nosto.attributes.header_image.data.attributes.alternativeText || '',
+          src: thumbnail?.url ? process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url : '',
+          alt: nosto.attributes.header_image.data?.attributes.alternativeText || '',
         },
         href: `/nostot/${nosto.id}`,
       };
