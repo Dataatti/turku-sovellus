97a2b8b52578500e2540fc7b9ace6738e678a8d7
Aleksi Väisänen <aleksivaisa@gmail.com>
2022-01-12 19:26:11 +0200

    Add ulkoinen linkki schema for Strapi

---
+++ strapi/app/src/api/ulkoinen-linkki/controllers/ulkoinen-linkki.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  ulkoinen-linkki controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ulkoinen-linkki.ulkoinen-linkki');
