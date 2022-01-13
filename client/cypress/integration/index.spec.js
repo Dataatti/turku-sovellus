c10e2def9d9aaef1f4680e7419d95cd82dafede8
Petro Silenius <petro@dataatti.io>
2022-01-13 20:03:58 +0200

    Check if index page has nostot, assert read more text changes on locale change

--- client/cypress/integration/index.spec.js
+++ client/cypress/integration/index.spec.js
@@ -4,13 +4,17 @@ describe('index', () => {
       method: 'GET',
       url: 'https://kerrokantasi-api.turku.fi/**',
     }).as('kerrokantasiData');
-
     cy.intercept({
       method: 'GET',
       url: 'https://api.turku.fi/linkedevents/v1/**',
     }).as('eventsData');
+
     cy.visit('/');
     cy.wait(['@kerrokantasiData', '@eventsData']);
+
+    cy.get('[data-testid="nostot-widget"]')
+      .find('li')
+      .should('have.length.greaterThan', 0);
 
     cy.get('[data-testid="turussa-tapahtuu-widget"]')
       .find('li')
@@ -28,13 +32,18 @@ describe('index', () => {
 
   it('should change content language', () => {
     cy.visit('/');
+    cy.contains('Lue lisää');
+
     cy.get('[data-testid=language-select]').click();
     cy.get('[data-value="en"]').click();
+
     cy.url().should('include', '/en');
-    // TODO: Check content language
+    cy.contains('Read more');
+
     cy.get('[data-testid=language-select]').click();
     cy.get('[data-value="sv"]').click();
+
     cy.url().should('include', '/sv');
-    // TODO: Check content language
+    cy.contains('Läs mer');
   });
 });
