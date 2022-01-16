cd93a153a2b51cb6a0006e1cf39364f8d2888938
Matias <matias.lappalainen@loikka.io>
2022-01-16 16:37:13 +0100

    Add async await to ulkoisetlinkit

--- client/hooks/useUlkoisetLinkit.ts
+++ client/hooks/useUlkoisetLinkit.ts
@@ -4,5 +4,8 @@ import { useQuery } from 'react-query';
 
 export const useUlkoisetLinkit = () => {
   const { locale } = useRouter();
-  return useQuery(['getUlkoisetLinkit', locale], () => strapiClient.ulkoisetLinkit.list(locale || 'fi'));
+  return useQuery(
+    ['getUlkoisetLinkit', locale],
+    async () => await strapiClient.ulkoisetLinkit.list(locale || 'fi')
+  );
 };
