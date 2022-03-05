d0e2795c94091b24573d0f8dd4a0e00641741279
Aleksi Väisänen <aleksivaisa@gmail.com>
2022-03-05 19:03:00 +0200

    TS-34 Add comment about Ulkoinen linkki redirection

--- client/pages/ulkoinenLinkki/[id].tsx
+++ client/pages/ulkoinenLinkki/[id].tsx
@@ -1,5 +1,11 @@
 import { GetServerSideProps } from 'next';
 
+/*
+  This page is purely for redirecting to home page.
+  strapi-plugin-preview-button plugin doesn't support "View live page" links without /[id] ending
+  Since Ulkoinen linkki is only rendered on the home page, this is to prevent showing 404 if navigated from Strapi  
+*/
+
 const UlkoinenLinkki = () => <div />;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
