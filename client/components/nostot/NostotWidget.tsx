5edca3af74e14487a2dabe6fb15482f937aecd15
Petro Silenius <petro@dataatti.io>
2022-03-27 21:31:49 +0300

    Hide nostot widget if no items are found

--- client/components/nostot/NostotWidget.tsx
+++ client/components/nostot/NostotWidget.tsx
@@ -2,41 +2,24 @@ import ListWidget from 'components/ListWidget';
 import { ListItemType } from 'components/ListWidget/ListWidgetItem';
 import { useRouter } from 'next/router';
 
-export const NostotWidget = ({ title, nostot }: { title: string, nostot: Nosto[] }) => {
+export const NostotWidget = ({ title, nostot }: { title: string; nostot: Nosto[] }) => {
   const { locale } = useRouter();
 
-  const errorMessages: { fi: string; en: string; sv: string } = {
-    fi: 'Nostoja ei saatavilla',
-    en: 'Highlights not available',
-    sv: '',
-  };
+  if (nostot.length === 0) return null;
 
-  const mapData = (data: Nosto[]) => {
-    const mappedNostot = data?.map((nosto) => {
-      const thumbnail = nosto.attributes.header_image.data?.attributes.formats.thumbnail;
-      return {
-        title: nosto.attributes.title,
-        description: nosto.attributes.description,
-        thumbnail: {
-          src: thumbnail?.url ? process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url : '',
-          alt: nosto.attributes.header_image.data?.attributes.alternativeText || '',
-        },
-        href: `/nostot/${nosto.id}`,
-        externalLink: false,
-      };
-    });
-    return mappedNostot;
-  };
-
-  const errorData = (locale: 'fi' | 'en' | 'sv'): ListItemType | undefined => {
-    if (nostot?.length === 0) {
-      return {
-        title: errorMessages[locale],
-        thumbnail: { src: undefined },
-      };
-    }
-    return undefined;
-  };
+  const mappedNostot = nostot?.map((nosto) => {
+    const thumbnail = nosto.attributes.header_image.data?.attributes.formats.thumbnail;
+    return {
+      title: nosto.attributes.title,
+      description: nosto.attributes.description,
+      thumbnail: {
+        src: thumbnail?.url ? process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url : '',
+        alt: nosto.attributes.header_image.data?.attributes.alternativeText || '',
+      },
+      href: `/nostot/${nosto.id}`,
+      externalLink: false,
+    };
+  });
 
   return (
     <ListWidget
@@ -44,9 +27,8 @@ export const NostotWidget = ({ title, nostot }: { title: string, nostot: Nosto[]
       title={title}
       readMoreHref="/nostot"
       variant="primary"
-      items={nostot && mapData(nostot)}
+      items={mappedNostot}
       isLoading={false}
-      error={errorData((locale || 'fi') as Lang)}
     />
   );
 };
