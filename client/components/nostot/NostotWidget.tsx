f7842a30c790751d79a48cd3065f48b0b883fe18
Petro Silenius <petro@dataatti.io>
2022-03-27 21:37:22 +0300

    Refactor to use same mapping logic as other components

--- client/components/nostot/NostotWidget.tsx
+++ client/components/nostot/NostotWidget.tsx
@@ -2,24 +2,27 @@ import ListWidget from 'components/ListWidget';
 import { ListItemType } from 'components/ListWidget/ListWidgetItem';
 import { useRouter } from 'next/router';
 
-export const NostotWidget = ({ title, nostot }: { title: string; nostot: Nosto[] }) => {
+export const NostotWidget = ({ title, nostot }: { title: string, nostot: Nosto[] }) => {
   const { locale } = useRouter();
 
   if (nostot.length === 0) return null;
 
-  const mappedNostot = nostot?.map((nosto) => {
-    const thumbnail = nosto.attributes.header_image.data?.attributes.formats.thumbnail;
-    return {
-      title: nosto.attributes.title,
-      description: nosto.attributes.description,
-      thumbnail: {
-        src: thumbnail?.url ? process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url : '',
-        alt: nosto.attributes.header_image.data?.attributes.alternativeText || '',
-      },
-      href: `/nostot/${nosto.id}`,
-      externalLink: false,
-    };
-  });
+  const mapData = (data: Nosto[]) => {
+    const mappedNostot = data?.map((nosto) => {
+      const thumbnail = nosto.attributes.header_image.data?.attributes.formats.thumbnail;
+      return {
+        title: nosto.attributes.title,
+        description: nosto.attributes.description,
+        thumbnail: {
+          src: thumbnail?.url ? process.env.NEXT_PUBLIC_STRAPI_URL + thumbnail.url : '',
+          alt: nosto.attributes.header_image.data?.attributes.alternativeText || '',
+        },
+        href: `/nostot/${nosto.id}`,
+        externalLink: false,
+      };
+    });
+    return mappedNostot;
+  };
 
   return (
     <ListWidget
@@ -27,7 +30,7 @@ export const NostotWidget = ({ title, nostot }: { title: string; nostot: Nosto[]
       title={title}
       readMoreHref="/nostot"
       variant="primary"
-      items={mappedNostot}
+      items={nostot && mapData(nostot)}
       isLoading={false}
     />
   );
