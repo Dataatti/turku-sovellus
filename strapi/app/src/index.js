3bb9c3d2cf38fdc689024e9b50fc691e7f4d86b6
Matias Lappalainen <matias.lappalainen@mehilainen.fi>
2022-04-07 22:10:54 +0300

    Add header seeders to strapi

--- strapi/app/src/index.js
+++ strapi/app/src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,5 +16,95 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    try {
+      const headers = [
+        {
+          id: 1,
+          locale: "fi",
+          text: "Nostot",
+          type: "nostot",
+        },
+        {
+          id: 2,
+          locale: "en",
+          text: "Highlights",
+          type: "nostot",
+        },
+        {
+          id: 3,
+          locale: "fi",
+          text: "Tiedotteet",
+          type: "tiedotteet",
+        },
+        {
+          id: 4,
+          locale: "en",
+          text: "Bulletin",
+          type: "tiedotteet",
+        },
+        {
+          id: 5,
+          locale: "fi",
+          text: "Liikennetiedotteet",
+          type: "liikennetiedotteet",
+        },
+        {
+          id: 6,
+          locale: "en",
+          text: "Traffic announcements",
+          type: "liikennetiedotteet",
+        },
+        {
+          id: 7,
+          locale: "fi",
+          text: "Turku sovellus",
+          type: "sovellus",
+        },
+        {
+          id: 8,
+          locale: "en",
+          text: "Turku application",
+          type: "sovellus",
+        },
+        {
+          id: 9,
+          locale: "fi",
+          text: "Kerrokantasi",
+          type: "kerrokantasi",
+        },
+        {
+          id: 10,
+          locale: "en",
+          text: "Voice your opinion",
+          type: "kerrokantasi",
+        },
+        {
+          id: 11,
+          locale: "fi",
+          text: "Tapahtumat",
+          type: "tapahtumat",
+        },
+        {
+          id: 12,
+          locale: "en",
+          text: "What's happening in Turku",
+          type: "tapahtumat",
+        },
+      ];
+
+      for (const header of headers) {
+        const h = await strapi
+          .controller("api::header.header")
+          .findOne({ params: header });
+
+          if (!h?.data) {
+          await strapi.controller("api::header.header").create({is: () => false, request: {query: {}, body: { data: header }}})
+          console.log("CREATED", header.type);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
